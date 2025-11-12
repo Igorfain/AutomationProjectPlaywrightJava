1f40e46738bb731c3d5848074c156634f267ce03
Igor <igorfain@gmail.com>
2025-11-12 16:27:20 +0200

    some api test fixes

--- src/test/java/apitests/tests/jsonplaceholderapi/GetTests.java
+++ src/test/java/apitests/tests/jsonplaceholderapi/GetTests.java
@@ -12,7 +12,7 @@ public class GetTests extends BaseTest {
 
     @Test
     public void testGetRequest() {
-        Response response = apiService.sendGetRequest("/posts/1");
+        Response response = apiService.sendGetRequest(JSON_PLACEHOLDER_URI+"/posts/1");
         System.out.println("Response Body is: " + response.getBody().asString());
 
         Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200");
--- src/test/java/apitests/tests/jsonplaceholderapi/PostTests.java
+++ src/test/java/apitests/tests/jsonplaceholderapi/PostTests.java
@@ -14,7 +14,7 @@ public class PostTests extends BaseTest {
     @Test
     public void testPostRequest() {
         String requestBody = JsonUtils.createPostRequestBody("foo", "bar", 1);
-        Response response = apiService.sendPostRequest("/posts", requestBody);
+        Response response = apiService.sendPostRequest(JSON_PLACEHOLDER_URI+"/posts", requestBody);
         System.out.println("Response Body is: " + response.getBody().asString());
 
         Assert.assertEquals(response.getStatusCode(), 201, "Status code should be 201");

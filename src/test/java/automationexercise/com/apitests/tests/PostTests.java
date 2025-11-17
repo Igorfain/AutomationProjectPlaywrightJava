335888a76e3a14da65a0c6c1cff59b5c8ddb2878
Igor <igorfain@gmail.com>
2025-11-17 11:43:17 +0200

    Search without any param-should get 400

--- src/test/java/automationexercise/com/apitests/tests/PostTests.java
+++ src/test/java/automationexercise/com/apitests/tests/PostTests.java
@@ -4,7 +4,9 @@ import automationexercise.com.apitests.BaseTest;
 import automationexercise.com.apitests.services.Endpoints;
 import automationexercise.com.apitests.helpers.ApiRequestHelper;
 import automationexercise.com.utils.ConfigPaths;
+import com.google.gson.Gson;
 import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.testng.Assert;
@@ -33,11 +35,12 @@ public class PostTests extends BaseTest {
 
         email = config.getString("username");
         password = config.getString("password");
+
     }
 
     @Test(description = "Search Product using keyword 'Men Tshirt'")
     @Story("Positive Test - Search for existing product")
-    @Severity(SeverityLevel.NORMAL)
+    @Tag("API")
     @Owner("Igor")
     public void testSearchProduct() {
         Response response = apiRequestHelper.sendPostFormRequest(AUTOMATION_EXERCISE_URI + Endpoints.SEARCH_PRODUCT, "search_product", "Men Tshirt");
@@ -49,7 +52,7 @@ public class PostTests extends BaseTest {
 
     @Test(description = "Verify Login with valid email and password")
     @Story("Positive Test - Verify existing user login")
-    @Severity(SeverityLevel.CRITICAL)
+    @Tag("API")
     @Owner("Igor")
     public void testVerifyLogin() {
         Map<String, String> formData = new HashMap<>();
@@ -60,6 +63,33 @@ public class PostTests extends BaseTest {
         Assert.assertEquals(response.getStatusCode(), 200, "Expected status code 200");
         Assert.assertTrue(response.getBody().asString().contains("User exists!"), "Response body should contain 'User exists!'");
 
+    }
+
+    @Test(description = "POST To Search Product without search_product parameter")
+    @Story("API Validation")
+    @Tag("API")
+    @Owner("Igor")
+    public void testValidateResponseCodeFromBody() {
+
+        // 1. Send POST request without parameter
+        Response response = apiRequestHelper.sendPostFormRequest(
+                AUTOMATION_EXERCISE_URI + Endpoints.SEARCH_PRODUCT,
+                "",
+                ""
+        );
+
+        String body = response.asString();
+
+        Map<String, Object> json = new Gson().fromJson(body, Map.class);
+
+        int apiCode = ((Number) json.get("responseCode")).intValue();
+        Assert.assertEquals(apiCode, 400, "Expected internal API responseCode = 400");
+
+        Assert.assertTrue(body.contains("Bad request"),
+                "Body should contain 'Bad request' message");
+
+        Assert.assertEquals(response.getStatusCode(), 200,
+                "HTTP status should always be 200 for this API");
     }
 
 }

bb3793def13f3354417b3ec84a66f02989117149
Igor <igorfain@gmail.com>
2025-11-20 12:45:39 +0200

    MainConfig changes

--- src/test/java/automationexercise/com/utils/ConfigPaths.java
+++ src/test/java/automationexercise/com/utils/ConfigPaths.java
@@ -1,6 +1,6 @@
 package automationexercise.com.utils;
 
 public class ConfigPaths {
-    public static final String MAIN_CONFIG_PATH = "src/test/java/automationexercise/com/infra/MainConfig.json";
+    public static final String MAIN_CONFIG_PATH = "src/test/resources/MainConfig.json";
     // Add more paths as needed
 }

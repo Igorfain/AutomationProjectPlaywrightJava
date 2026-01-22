5f5b8fa81e11e565f25e9c906ca3f1177ed11917
Igor <igorfain@gmail.com>
2026-01-22 11:33:38 +0200

    Fix steps reflection in allure

--- src/test/java/automationexercise/com/steps/ContactUsSteps.java
+++ src/test/java/automationexercise/com/steps/ContactUsSteps.java
@@ -14,21 +14,21 @@ public class ContactUsSteps {
 
     @Step("Click on 'Contact Us' button")
     public ContactUsSteps clickContactUsButton() {
-        ConsoleReporter.log("STEP: Click on 'Contact Us' button");
+        ConsoleReporter.log("Click on 'Contact Us' button");
         contactUsPage.clickContactUsButton();
         return this;
     }
 
     @Step("Verify 'Get In Touch' is existing in Contact Us page")
     public ContactUsSteps verifyGetInTouchText() {
-        ConsoleReporter.log("STEP: Verify 'Get In Touch' is existing in Contact Us page");
+        ConsoleReporter.log("Verify 'Get In Touch' is existing in Contact Us page");
         contactUsPage.verifyGetInTouchText();
         return this;
     }
 
     @Step("Fill the relevant fields ")
     public ContactUsSteps fillRelevantFields(String name,String email,String subject,String message) {
-        ConsoleReporter.log("STEP: Fill the relevant fields");
+        ConsoleReporter.log("Fill the relevant fields");
         contactUsPage
                 .insertName(name)
                 .insertEmail(email)
@@ -39,35 +39,35 @@ public class ContactUsSteps {
 
     @Step("Upload file in Contact Us form")
     public ContactUsSteps uploadFile(String filePath) {
-        ConsoleReporter.log("STEP: Upload file in Contact Us form");
+        ConsoleReporter.log("Upload file in Contact Us form");
         contactUsPage.uploadFile(filePath);
         return this;
     }
 
     @Step("Click on 'Submit' button")
     public ContactUsSteps clickSubmitButton() {
-        ConsoleReporter.log("STEP: Click on 'Submit' button");
+        ConsoleReporter.log("Click on 'Submit' button");
         contactUsPage.clickSubmitButton();
         return this;
     }
 
     @Step("Verify success message is visible after form submission")
     public ContactUsSteps verifySuccessMessage() {
-        ConsoleReporter.log("STEP: Verify success message is visible after form submission");
+        ConsoleReporter.log("Verify success message is visible after form submission");
         contactUsPage.verifySuccessMessage();
         return this;
     }
 
     @Step("Click Home button after success message")
     public ContactUsSteps clickHomeButton() {
-        ConsoleReporter.log("STEP: Click Home button after success message");
+        ConsoleReporter.log("Click Home button after success message");
         contactUsPage.clickHomeButton();
         return this;
     }
 
     @Step("Verify navigation to Home page")
     public ContactUsSteps verifyNavigatedToHomePage() {
-        ConsoleReporter.log("STEP: Verify navigation to Home page");
+        ConsoleReporter.log("Verify navigation to Home page");
         contactUsPage.verifyNavigatedToHomePage();
         return this;
     }

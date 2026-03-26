11de37dfe09f527328424f16aac4ea44587de99d
Igor <igorfain@gmail.com>
2026-03-26 20:08:19 +0200

    Refactor elements variables to constant

--- src/test/java/saucedemo/com/pages/LoginPage.java
+++ src/test/java/saucedemo/com/pages/LoginPage.java
@@ -5,12 +5,11 @@ import saucedemo.com.infra.actions.ActionBot;
 
 public class LoginPage extends BasePage {
 
-    // Locators
-    private String usernameSelector = "#user-name";
-    private String passwordSelector = "#password";
-    private String loginButtonSelector = "#login-button";
-    private String errorMessageSelector = "div.error-message-container";
-    private ActionBot actionBot;
+    private static final String USERNAME_SELECTOR = "#user-name";
+    private static final String PASSWORD_SELECTOR = "#password";
+    private static final String LOGIN_BUTTON_SELECTOR = "#login-button";
+    private static final String ERROR_MESSAGE_SELECTOR = "div.error-message-container";
+    private final ActionBot actionBot;
 
     public LoginPage(Page page) {
         super(page); // Call the constructor of BasePage
@@ -20,16 +19,16 @@ public class LoginPage extends BasePage {
     // With the ActionBot, we can now use the fill method to fill in the username
     public void enterUsername(String username) {
         //  page.locator(usernameSelector).fill(username);  *//example to use actionBot - Igor
-        actionBot.fill(usernameSelector, username);
+        actionBot.fill(USERNAME_SELECTOR, username);
     }
 
     // Without the ActionBot, we would have to use the fill method directly on the page object-password field.
     public void enterPassword(String password) {
-        page.locator(passwordSelector).fill(password);
+        page.locator(PASSWORD_SELECTOR).fill(password);
     }
 
     public void clickLoginButton() {
-        page.locator(loginButtonSelector).click();
+        page.locator(LOGIN_BUTTON_SELECTOR).click();
 
 
     }
